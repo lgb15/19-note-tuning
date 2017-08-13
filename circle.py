593f5839577923d2ce2bb3813bc04256a6193535
Prasenjit Saha <psaha@physik.uzh.ch>
2017-08-13 08:25:52 +0200

    littles

--- circle.py
+++ circle.py
@@ -13,32 +13,25 @@ x = 1.1*np.array(x)
 y = 1.1*np.array(y)
 pl.plot(x,y,'o',color='black',markersize=10)
 
+
+h = [1,3,5,7,9]
+c = ['yellow','cyan','blue','magenta','red']
 r = []
-def just(r,h,col):
-    for p in h:
-        for q in h:
-            if p >= q:
-                f = Fraction(p,q)
-                while f < 1:
-                    f *= 2
-                while f > 2:
-                    f /= 2
-                if f not in r:
-                    r.append(f)
-                    lf = float(f)   
-                    lf = np.log(lf)/np.log(2)
-                    print(f,lf)
-                    a = 2*np.pi*(1/4-lf)
-                    x = np.cos(a)
-                    y = np.sin(a)
-                    pl.plot(x,y,'o',color=col,markersize=10,
-                            markeredgewidth=0) 
 
-just(r,[1,3],'cyan')
-just(r,[1,3,5],'blue')
-just(r,[1,3,5,7],'magenta')
-just(r,[1,3,5,7,9],'red')
-
+for i,p in enumerate(h):
+    for q in h:
+        if p >= q:
+            f = Fraction(p,q)
+            if f not in r:
+                r.append(f)
+                lf = float(f)   
+                lf = np.log(lf)/np.log(2)
+                print(f)
+                a = 2*np.pi*(1/4-lf)
+                x = np.cos(a)
+                y = np.sin(a)
+                pl.plot(x,y,'o',color=c[i],markersize=10,
+                        markeredgewidth=0) 
 
 pl.gca().set_aspect('equal')
 pl.show()

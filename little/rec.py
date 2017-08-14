e0bf98c99c0e48a8da06353eb7b635a77930f741
Prasenjit Saha <psaha@physik.uzh.ch>
2017-08-14 11:15:58 +0200

    rec

---
+++ little/rec.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.insert(0, '../')
+from synthesizer import synthesizer
+
+#synthesizer('d9t.txt')
+#synthesizer('td9t.txt')
+#synthesizer('thirds.txt')
+#synthesizer('corsican.txt')
+synthesizer('cluster.txt')
+
